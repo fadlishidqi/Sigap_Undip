e702a7a436b95896b685c7ad468840c798afc27c
fadlishidqi <fadlishidqifirdaus@gmail.com>
2025-06-20 00:50:11 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ['firebasestorage.googleapis.com', 'sigap-api-5hk6r.ondigitalocean.app'],
   },
 };
 
